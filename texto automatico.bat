3081561dce7b248a8d3aeae613d558448fcfb354
Nycolas Santana <78886462+NycolasSM@users.noreply.github.com>
2023-03-07 20:03:31 -0300

    Delete texto automatico.bat

--- texto automatico.bat
+++
@@ -1,8 +0,0 @@
-@color f0
-@title Criando documento
-@echo Criando pasta de arquivos
-@md "c:\Meu documento"
-@echo Criando arquivo, aguarde...
-@echo "Meu Texto automatico">> "c:\Meu documento\Texto.txt"
-@echo Fim, ate logo!
-@pause>nul
